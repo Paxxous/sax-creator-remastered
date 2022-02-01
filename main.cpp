9dc4dd27697df327572c3581594e2e4da5d6a755
Paxxous <itsenveehio@gmail.com>
2022-01-31 19:16:24 -0500

    setting up

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+//Main, fetch the commands and then process them
+int main() {
+  int openOrCreate;
+  std::cout << "Would you like to create (1) or open (2) a project?\n";
+
+  std::cin >> openOrCreate;
+}
